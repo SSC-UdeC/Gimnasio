5850824933a02085d33874a3dcba10d5773cfb4e
Aval0G <avaldez0@ucol.mx>
2024-10-10 12:37:07 -0600

    configuracion y ruta para la creacion de la bd

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using SSC_Gimnasio;
+using SSC_Gimnasio.Modelos;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddDbContext<GymnasioDBContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConecction")));
 await builder.Build().RunAsync();
