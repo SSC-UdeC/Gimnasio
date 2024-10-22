0cd6bfabc56c703e9ae372425da452b17d720997
Aval0G <avaldez0@ucol.mx>
2024-10-22 12:10:18 -0600

    Ya se puede añadir un cliente

--- SSC_Gimnasio/Repositorio/IRepositorioClientes.cs
+++ SSC_Gimnasio/Repositorio/IRepositorioClientes.cs
@@ -4,10 +4,10 @@ namespace SSC_Gimnasio.Repositorios
 {
     public interface IRepositorioClientes
     {
-        Task<List<Clientes>> GetAll(); // Get all clients
-        Task<Clientes?> Get(int id); // Get a client by ID
-        Task<Clientes> Add(Clientes cliente); // Create a new client
-        Task Update(int id, Clientes cliente); // Update an existing client
-        Task Delete(int id); // Delete a client by ID
+        Task<List<Clientes>> GetAll(); 
+        Task<Clientes?> Get(int id); 
+        Task<Clientes> Add(Clientes cliente); 
+        Task Update(int id, Clientes cliente); 
+        Task Delete(int id); 
     }
 }
