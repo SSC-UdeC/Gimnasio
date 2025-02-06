c4f4c6fbe904ddcbc6316f1ab0998549dbed8c58
Valdez Gutierrez Aldo Eduardo <avaldez0@ucol.mx>
2025-02-06 12:51:01 -0600

    Añadida y probada la funcionalidad para descargar un QR

--- SSC_Gimnasio/wwwroot/descargarArchivo.js
+++ SSC_Gimnasio/wwwroot/descargarArchivo.js
@@ -1,10 +1,7 @@
-window.descargarArchivo = function (nombreArchivo, byteArray) {
-    const blob = new Blob([byteArray], { type: "image/png" });
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = nombreArchivo;
-    document.body.appendChild(link);
+window.descargarArchivo = (fileName, byteArray) => {
+    const blob = new Blob([new Uint8Array(byteArray)], { type: "image/png" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
     link.click();
-    document.body.removeChild(link);
-    window.URL.revokeObjectURL(link.href);
 };
