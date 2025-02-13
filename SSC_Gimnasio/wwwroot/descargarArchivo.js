78636ce232d23db4c366683cb891814e3a53e0da
Valdez Gutierrez Aldo Eduardo <avaldez0@ucol.mx>
2025-02-13 12:43:29 -0600

    Mejora en el Qr

--- SSC_Gimnasio/wwwroot/descargarArchivo.js
+++ SSC_Gimnasio/wwwroot/descargarArchivo.js
@@ -1,7 +1,18 @@
-window.descargarArchivo = (fileName, byteArray) => {
+window.descargarArchivoQr = (fileName, byteArray) => {
     const blob = new Blob([new Uint8Array(byteArray)], { type: "image/png" });
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
     link.download = fileName;
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
+};
+
+window.descargarArchivoPdf = (filename, mimeType, base64Data) => {
+    const link = document.createElement("a");
+    link.href = `data:${mimeType};base64,${base64Data}`;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 };
--- SSC_Gimnasio/wwwroot/descargarpdf.js
+++
@@ -1,8 +0,0 @@
-function downloadFile(filename, mimeType, base64Data) {
-    const link = document.createElement("a");
-    link.href = `data:${mimeType};base64,${base64Data}`;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
